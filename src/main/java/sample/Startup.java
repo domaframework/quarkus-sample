86bbf1f0ccaa741e6ac4a151631e2ca8dc4a18e6
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2020-06-20 17:40:16 +0900

    Simplify the startup logic

--- src/main/java/sample/Startup.java
+++ src/main/java/sample/Startup.java
@@ -7,16 +7,12 @@ import java.util.Objects;
 
 public class Startup {
   private final ScriptDao scriptDao;
-  private final MessageRepository messageRepository;
 
-  public Startup(ScriptDao scriptDao, MessageRepository messageRepository) {
+  public Startup(ScriptDao scriptDao) {
     this.scriptDao = Objects.requireNonNull(scriptDao);
-    this.messageRepository = Objects.requireNonNull(messageRepository);
   }
 
   void onStart(@Observes StartupEvent event) {
     scriptDao.create();
-    messageRepository.insert(new Message(1, "hello", new Locale("en", "US")));
-    messageRepository.insert(new Message(2, "世界", new Locale("ja", "JP")));
   }
 }
